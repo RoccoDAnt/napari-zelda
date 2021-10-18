c63cd3e3c037272c59d5cb2931745f1b34d6353d
RoccoDAnt <rocco.dantuono@hotmail.it>
2021-10-18 14:05:15 +0100

    include package data

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 use_scm={"write_to": "napari_zelda/_version.py"}
 
 setup(
-    version='0.1.1')
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True
+    )
