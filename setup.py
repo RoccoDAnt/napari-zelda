1f1d9ffa7780365a3f6bbbbd630160e9c8d794bb
RoccoDAnt <rocco.dantuono@hotmail.it>
2021-10-19 13:07:05 +0100

    v

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 use_scm={"write_to": "napari_zelda/_version.py"}
 
 setup(
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True
     )
