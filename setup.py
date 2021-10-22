6c6ab7520a88a36a7df8ceee794632066206ad20
RoccoDAnt <rocco.dantuono@hotmail.it>
2021-10-22 18:17:28 +0100

    minor

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
+use_scm={"write_to": "napari_zelda/_version.py"}
+
 setup(
-    name='napari-zelda',    
+    name='napari-zelda',
     version='0.1.2',
     author="Rocco D'Antuono, Giuseppina Pisignano",
     description="ZELDA: a 3D Image Segmentation and Parent-Child relation plugin for microscopy image analysis in napari",
